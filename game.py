638ded6bdc9876294d5acaa285c2bee37612829e
Jason Reburn <70227590+rebj2977@users.noreply.github.com>
2022-10-06 13:36:30 -0400

    Update game.py

--- game.py
+++ game.py
@@ -21,7 +21,7 @@ except:
 # Parts worth reading #
 #######################
 
-class Agentqweqweqwe:
+class Agent:
   """
   An agent must define a getAction method, but may also define the
   following methods which will be called if they exist:
